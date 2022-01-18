81cfacd6178af47b8606f33d266d3ea2501203f2
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-18 19:21:01 +0500

    kjhg

--- script.js
+++ script.js
@@ -217,6 +217,8 @@ dt2 = new Date("October 19, 2014 11:13:00");
 console.log(diff_weeks(dt1, dt2));
 
 
+
+
 // Write a JavaScript function to get time differences in months between two dates.   
 // Test Data :
 // dt1 = new Date("June 13, 2014 08:11:00");
